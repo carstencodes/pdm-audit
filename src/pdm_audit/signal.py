0d69c52aaf350b46a76efc632dcb44ba90f9898a
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2024-03-23 22:12:37 +0100

    fix: hooks argument is no longer ignored
    Use kw args for resolving unknown arguments passed to the hook

--- src/pdm_audit/signal.py
+++ src/pdm_audit/signal.py
@@ -4,7 +4,8 @@ from pdm.project import Project
 from .plugin import Auditor
 
 
-def run_pdm_audit_signal(project: Project, _: dict, dry_run: bool):
+def run_pdm_audit_signal(project: Project, candidates: dict, dry_run: bool, **kwargs) -> None:
+    del candidates, kwargs
     run: bool = project.config["tools.pdm.audit_plugin.post_install_hook"] or False
     if not dry_run and run:
         auditor: Auditor = Auditor()
