5b30a6ea79459dc4b31f978f3d15b62375df10b0
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2025-06-03 18:56:56 +0000

    fix: Harmonize logging output for plugin and signal

--- src/pdm_audit/signal.py
+++ src/pdm_audit/signal.py
@@ -12,6 +12,11 @@
 
 from pdm.project import Project
 
+from pdm_pfsc.logging import (
+    setup_logger,
+    update_logger_from_project_ui,
+)
+
 from .config import Config  
 from .plugin import Auditor
 
@@ -22,6 +27,8 @@ def run_pdm_audit_signal(
     del packages, kwargs
     config = Config(project)
     run: bool = config.use_hook
+    setup_logger(config.hook_verbose)
+    update_logger_from_project_ui(project.core.ui)
     if not dry_run and run:
         auditor: Auditor = Auditor()
         auditor.audit(
